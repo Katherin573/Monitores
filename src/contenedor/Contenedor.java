fc75533d8d3a00721f231e4f5298023c168a84e8
Katherin573 <61336687+Katherin573@users.noreply.github.com>
2020-05-22 19:05:02 -0600

    Monitores

---
+++ src/contenedor/Contenedor.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contenedor;
+
+/**
+ *
+ * @author usuario
+ */
+public class Contenedor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+  
+
+    private int contenido;
+    private boolean contenedorlleno = Boolean.FALSE;
+ 
+    /**
+     * Obtiene de forma concurrente o síncrona el elemento que hay en el contenedor
+     * @return Contenido el contenedor
+     */
+    public synchronized int get()
+    {
+        while (!contenedorlleno)
+        {
+            try
+            {
+                wait();
+            } 
+            catch (InterruptedException e) 
+            {
+                System.err.println("Contenedor: Error en get -> " + e.getMessage());
+            }
+        }
+        contenedorlleno = Boolean.FALSE;
+        notify();
+        return contenido;
+    }
+ 
+    /**
+     * Introduce de forma concurrente o síncrona un elemento en el contenedor
+     * @param value Elemento a introducir en el contenedor
+     */
+    public synchronized void put(int value) 
+    {
+        while (contenedorlleno) 
+        {
+            try
+            {
+                wait();
+            } 
+            catch (InterruptedException e) 
+            {
+                System.err.println("Contenedor: Error en put -> " + e.getMessage());
+            }
+        }
+        contenido = value;
+        contenedorlleno = Boolean.TRUE;
+        notify();
+    }
+}
